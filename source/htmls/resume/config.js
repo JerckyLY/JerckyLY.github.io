7c08e9cb6707fde412f0c0b825c35c64b619dbb3
jercky <1743877040@qq.com>
2021-09-08 09:54:14 +0800

    简历修整

---
+++ source/htmls/resume/config.js
@@ -0,0 +1,167 @@
+//工作经历配置
+const WorkConfig = [
+
+    {
+        companyName:'河南国测信息技术有限公司',
+        jobName:'GIS研发工程师',
+        workDate:'2018.05 -- 至今',
+        list:[
+            {
+                title:'智慧卫东一张图',
+                des:'实现对加格达奇区卫东街道的相关空间信息展示。基于Cesium + Openlayers 对卫东数据的倾斜摄影数据，分层分户数据，地下管线数据以及相关空间业务数据，在二三维一张图中进行展示，分析，统计等。',
+                part:'负责项目的前端架构搭建，以及二三维地图界面编写。同时处理相关三维数据，用于系统展示。',
+                img:[
+                    './resume/img/wd/1.png',
+                    './resume/img/wd/2.png',
+                    './resume/img/wd/3.png',
+                    './resume/img/wd/4.png',
+
+                ]
+            },
+            {
+                title:'智慧三维矿山监管信息系统',
+                des:'实现矿山三维模型的加载展示，提供测量等分析功能，进行矿山进行监控管理。',
+                part:'负责项目的前端架构搭建，三维模式数据处理，界面编写和调整。系统部署。',
+                img:[
+                    './resume/img/ks/1.png',
+                    './resume/img/ks/2.png',
+                    './resume/img/ks/3.png',
+                    './resume/img/ks/4.png',
+                    './resume/img/ks/5.png',
+                    './resume/img/ks/6.png',
+                ]
+            },
+            {
+                title:'鹤壁市淇滨区创新区园区三维管理平台',
+                des:'对园区的模型加载显示，与摄像头数据，停车道闸，路灯数据对接展示。',
+                part:'模型数据处理，界面编写和系统部署。',
+                img:[
+                    './resume/img/hb/1.png',
+                    './resume/img/hb/2.png',
+                    './resume/img/hb/3.png',
+                ]
+            },
+            {
+                title:'勘探公司地理信息系统',
+                des:'基于 mapboxgl，ant-design-vue 前端展示框架，postgis 矢量切片服务支持，对石油管道及相关数据展示，查询分析。',
+                part:'主要项目二维地图部分，三维部分展示',
+                img:[
+                    './resume/img/yc/1.png',
+                    './resume/img/yc/2.png',
+                    './resume/img/yc/3.png',
+                    './resume/img/yc/4.png',
+                    './resume/img/yc/5.png',
+                    './resume/img/yc/6.png',
+                    './resume/img/yc/7.png',
+                    './resume/img/yc/8.png',
+                ]
+            },
+            {
+                title:'上海农业遥感监测系统',
+                des:'对上海农科院提供的实验田的空间数据展示，对接监测设备，展示设备数据',
+                part:'负责项目的前端开发，和系统部署。',
+                img:[
+                    './resume/img/shnky/1.png',
+                    './resume/img/shnky/2.png',
+                    './resume/img/shnky/3.png',
+                    './resume/img/shnky/4.png',
+                ]
+            },
+            {
+                title:'广州航道局控制点桌面管理系统',
+                des:'用于管理广州航道分布的 GPS 控制点和水准点，基于 openlayers 展示控制点分布，空间查询和统计，控制点信息导出文档,点的编辑等。',
+                part:'负责项目的前后端开发，和系统部署。',
+                img:[
+                    './resume/img/hangdao/1.png',
+                    './resume/img/hangdao/2.png',
+                    './resume/img/hangdao/3.png',
+                ]
+            },
+            {
+                title:'大地坐标转换软件',
+                des:'基于 Qt、 QGIS 针对国家统一使用 2000 大地坐标系政策，用于 wgs84 ,西安 80,北京 54 转换成 2000 国家大地坐标系。',
+                part:'负责软件的坐标数据文件读取和计算七参数和四参数，导出成txt格式。',
+                img:[
+                ]
+            }
+        ]
+    },
+    {
+        companyName:'河南汉威电子',
+        jobName:'GIS研发工程师',
+        workDate:'2017.07-2018.05',
+        list: [
+            {
+                title:'鹤壁浚县管网监测',
+                des:'基于 arcgis api for js ，一张图展示浚县管网分布，水表压力检测和报警。',
+                part:'负责地图中的空间查询和属性查询，测量，数据详情展示。',
+                img:[
+                ]
+            },
+            {
+                title:'智慧社区',
+                des:'基于 arcgis api for js ，对正弘数码港小区试点楼层分布展示。',
+                part:'负责地图中的空间查询和属性查询，图层控制，数据详情展示。',
+                img:[
+                ]
+            }
+        ]
+    }
+
+]
+
+//技能
+const SkillConfig = [
+    {
+        title:  'html、 js、css、jquery、Vue'
+    },
+    {
+        title:  '了解 webpack 打包、ES6 语法、less 语法'
+    },
+    {
+        title: 'Arcgis api for js （3.x）、openlayers、mapboxgl 、Cesium 前端 GIS 渲染框架',
+    },
+    {
+        title: 'ElementUI、ant-design-vue 前端 vue 展示框架',
+    },
+    {
+        title:  'postgis 入库 postgresql、postgis 函数返回矢量切片',
+    },
+    {
+        title:   'ArcMap 基本数据处理，ArcServer 服务发布 , geoserver 发布服务, CesiumLab 三维数据处理',
+    },
+    {
+        title:    '熟悉 OGC 服务标准，WFS,WMS,WTMS,TMS,WCS',
+    },
+    {
+        title:    '了解 linux 基础操作命令，Nginx 资源代理和跨域配置',
+    },
+    {
+        title:    '个人博客地址：',
+        url: 'https://jercky.top/'
+    },
+    {
+        title:   'Npm 个人上传库 mapboxgl-measure-tool、mapboxgl-print-tool'
+    },
+    {
+        title:   '三维功能整理展示：',
+        url: 'http://jerckly.gitee.io/cesium-view/#/'
+    },
+
+]
+
+//获奖
+const AwardConfig = [
+    {
+        title:'2014国家励志奖学金。'
+    },
+    {
+        title:'2015个人奖学金。'
+    },
+    {
+        title:'2016国家励志奖学金，2016学习进步奖学金。'
+    },
+    {
+        title:'2020年国土资源（广东）科学技术二等奖。'
+    },
+]
---
+++ source/htmls/resume/index.js
@@ -0,0 +1,172 @@
+// 在 #app 标签下渲染一个按钮组件
+// Vue.use(vant.Lazyload);
+// const imgLists = {
+//     hangdao:[
+//         './resume/img/hangdao/1.png',
+//         './resume/img/hangdao/2.png',
+//         './resume/img/hangdao/3.png',
+//     ],
+//     wd:[
+//         './resume/img/wd/1.png',
+//         './resume/img/wd/2.png',
+//         './resume/img/wd/3.png',
+//         './resume/img/wd/4.png',
+//     ]
+// }
+new Vue({
+    el: '#app',
+    data(){
+        return{
+            checked:true,
+            isClick: true,
+            currentIndex:0,
+            scroll: '',
+            navList:[],
+            WorkConfig,
+            SkillConfig,
+            AwardConfig
+        }
+    },
+    computed:{
+        age(){
+            const current = new Date().getFullYear()
+            const old = new Date('1994').getFullYear()
+            return current - old
+        },
+        work(){
+            const current = new Date().getFullYear()
+            const old = new Date('2017').getFullYear()
+            return current - old
+        }
+    },
+
+    methods:{
+        //照片统一做水印处理
+        handleImgList(){
+            Object.keys(imgLists).forEach(key=>{
+                this.imgList[key] = []
+                imgLists[key].forEach(url=>{
+                   this.getImgUrl({
+                       url,
+                       content:'Jercky 的简历展示',
+                       cb:(base64)=>{
+                           this.imgList[key].push(base64)
+                       }
+                   })
+                })
+            })
+        },
+        //图片添加水印
+        getImgUrl ({
+                       url = '',
+                       textAlign = 'left',
+                       textBaseline = 'top',
+                       font = '18px Microsoft Yahei',
+                       fillStyle = 'rgba(255, 255, 255, 0.5)',
+                       content = '我是默认的水印参数',
+                       cb = null,
+                       textX = 100,
+                       textY = 30
+                   } = {}) {
+            // 创建所需要添加水印的img图片
+            const img = new Image()
+            img.src = url
+            img.crossOrigin = 'anonymous'
+            img.onload = function () {
+                // 创建canvas，并将创建的img绘制成canvas
+                const canvas = document.createElement('canvas')
+                canvas.width = img.width
+                canvas.height = img.height
+                const ctx = canvas.getContext('2d')
+
+                ctx.drawImage(img, 0, 0)
+                ctx.textAlign = textAlign
+                ctx.textBaseline = textBaseline
+                ctx.font = font
+                ctx.fillStyle = fillStyle
+                ctx.rotate((Math.PI / 180) * 15)
+                // 循环绘制水印
+                // ctx.fillText(content, img.width - textX, img.height - textY)
+                for (let i = 0; i < img.height / 120; i++) {
+                    for (let j = 0; j < img.width / 50; j++) {
+                        ctx.fillText(content, i * 200, j * 100, img.width)
+                    }
+                }
+                // 将绘制完成的canvas转换为base64的地址
+                const base64Url = canvas.toDataURL()
+                cb && cb(base64Url)
+            }
+        },
+        mousedown() {
+            this.isClick = true;
+        },
+        mousemove() {
+            this.isClick = false;
+        },
+        //预览图片
+        previewImg(images,index){
+            vant.ImagePreview({
+                images,
+                showIndex:true,
+                loop:true,
+                startPosition:index,
+                showIndicators:true,
+                closeable: true,
+            })
+        },
+        getNav(){
+            const navs = document.getElementsByTagName('h2');
+            const list = []
+            for (let i = 0; i < navs.length; i++) {
+                let item = {}
+                item.name = navs[i].innerText
+                list.push(item)
+            }
+            this.navList = list
+        },
+        jump(index){
+           this.currentIndex = index
+            let jump = document.getElementsByClassName('main-view');
+            // 获取需要滚动的距离
+            let total = jump[index].offsetTop - 90;
+            // // Chrome
+            // document.body.scrollTop = total;
+            // // Firefox
+            // document.documentElement.scrollTop = total;
+            // // Safari
+            // window.pageYOffset = total;
+            //滚动动画
+            window.scrollTo({top:total,behavior:'smooth'})
+        },
+        dataScroll(){
+            this.scroll = document.documentElement.scrollTop || document.body.scrollTop;
+        },
+        loadScroll(){
+            const sections = document.getElementsByClassName('main-view');
+            for (let i = sections.length - 1; i >= 0; i--) {
+                if (this.scroll >= sections[i].offsetTop - 90) {
+                    //我在上面规定了每个el-tab-pane标签的name属性值为tab+该标签的index值
+                    this.currentIndex = i
+                    break;
+                }
+            }
+        }
+    },
+    watch:{
+        scroll(){
+            this.loadScroll()
+        }
+    },
+    mounted(){
+        window.addEventListener('scroll', this.dataScroll);
+        //获取导航列表
+        this.getNav()
+        // 水印
+        // this.handleImgList()
+
+    }
+});
+
+// 调用函数组件，弹出一个 Toast
+// 通过 CDN 引入时不会自动注册 Lazyload 组件
+// 可以通过下面的方式手动注册
